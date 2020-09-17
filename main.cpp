08737309a1e94b2e5f3905ac86a185569b4dab36
PaulOwO <paul.breuza@gmail.com>
2020-09-17 17:12:44 +0200

    first commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <windows.h>
+#include <iostream>
 
 int main()
 {
-	MessageBox(nullptr, "World!", "Hello", MB_ICONEXCLAMATION);
+	std::cout <<"Chifoumi!(type rock=1, scisors=2 or paper=3)\n";
+	int ChoixJoueur = 0;
+	int ChoixBot = 1; //bot prend toujours pierre
+	std::sin >> ChoixJoueur;
+
+	int rock << 1;
+	int scisors << 2;
+	int paper << 3;
+
+	if { ChoixJoueur = 1}
+	
+	
+	return 0;
+		
+
+
+
+
+
+	
 }
+
